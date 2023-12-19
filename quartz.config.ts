a17f3fe9ccddeb2618b9c890ef2eeabb9879c29d
Stuart Johnson <stuart@thestu.art>
2023-12-19 12:40:06 +0000

    no message

--- quartz.config.ts
+++ quartz.config.ts
@@ -52,6 +52,7 @@ const config: QuartzConfig = {
       }),
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.Description(),
